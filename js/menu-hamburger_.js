5ff6c64051c3893c4e5be29f10d27bea6a6d578c
marioajuez <marioajuez@gmail.com>
2021-03-12 14:19:24 -0500

    reconstruccion plantilla en archivo llamado sidenav_.html

--- js/menu-hamburger_.js
+++ js/menu-hamburger_.js
@@ -1,7 +1,5 @@
 const menuHamburger = document.querySelector(".menu-hamburger");
-// const nav = document.getElementById("menu-responsive").querySelector(":scope > nav");
 const nav = document.querySelector("#menu-responsive")
-
 
 menuHamburger.addEventListener("click", () => {
     menuHamburger.classList.toggle("open")
--- js/menu-nav-options.js
+++ js/menu-nav-options.js
@@ -1,9 +1,7 @@
 document.body.onload = useIt;
-console.log("hola")
 // document.addEventListener("DOMContentLoaded",useIt)
 
 function useIt() {
-  console.log("hola")
   let content = document.querySelector("template").content;
   var menu = document.querySelectorAll(".menu");
 
